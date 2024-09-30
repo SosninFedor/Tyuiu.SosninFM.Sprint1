d44f6a360fd9d3cba6ebdfe5887dac62134fc265
федор соснин <sosninfedor@gmail.com>
2024-09-30 22:08:37 +0500

    Task5

--- Tyuiu.SosninFM.Sprint1.Task5.V1.Lib/DataService.cs
+++ Tyuiu.SosninFM.Sprint1.Task5.V1.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.SosninFM.Sprint1.Task5.V1.Lib
     {
         public int DistanceBetweenDots(double x1, double y1, double x2, double y2)
         {
+           
             var res = Convert.ToInt32(Math.Abs(Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)))));
             return res;
         }
